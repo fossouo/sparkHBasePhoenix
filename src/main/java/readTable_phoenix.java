011e5022d1fa290a4d17f6a5fbd232de76a68aae
Donald Fossouo <dfossouo@hortonworks.com>
2019-11-28 09:10:15 +0100

    first commit

---
+++ src/main/java/readTable_phoenix.java
@@ -0,0 +1,84 @@
+import org.apache.spark.sql.*;
+import org.apache.spark.SparkContext;
+import org.apache.phoenix.spark.*;
+import org.apache.phoenix.spark.DefaultSource;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.security.UserGroupInformation;
+
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HConstants;
+import org.apache.hadoop.security.UserGroupInformation;
+import org.apache.phoenix.query.QueryServices;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SQLContext;
+
+import org.apache.phoenix.jdbc.PhoenixDriver;
+
+import com.google.common.collect.ImmutableMap;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class readTable_phoenix {
+    public static void main(String[] args){
+
+        try {
+            UserGroupInformation.setConfiguration(new Configuration());
+            UserGroupInformation.loginUserFromKeytab("hbase/dfossouo-1.dfossouo.root.hwx.site@ROOT.HWX.SITE",
+                    "/tmp/hbase.keytab");
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        //Create a SparkContext to initialize
+        String warehouseLocation = new File("spark-warehouse").getAbsolutePath();
+        SparkConf conf = new SparkConf();
+        conf.setAppName("ECLoad").setMaster("local").set("spark.sql.warehouse.dir", warehouseLocation);
+
+        conf.set("spark.testing.memory", "2147480000");         // if you face any memory issue
+
+       System.out.println("Using JDBC Driver");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        SQLContext sqlContext = new SQLContext(sc);
+
+        DataFrame readPhoenix = sqlContext.read().format("org.apache.phoenix.spark")
+                .options(ImmutableMap.of("driver", "org.apache.phoenix.jdbc.PhoenixDriver", "zkUrl",
+                        "jdbc:phoenix:dfossouo-2.dfossouo.root.hwx.site:2181:/hbase:hbase/dfossouo-1.dfossouo.root.hwx.site@ROOT.HWX.SITE:/tmp/hbase.keytab", "table", "WEB_STAT"))
+                .load();
+
+        System.out.println("Here is the schema of Phoenix Table");
+        readPhoenix.printSchema();
+        System.out.println("Here is the body of Phoenix Table");
+        readPhoenix.show();
+        System.out.println("End of JDBC Driver");
+
+        /*
+
+                readPhoenix.write().format("org.apache.phoenix.spark").mode(SaveMode.Overwrite)
+        .options(ImmutableMap.of("driver", "org.apache.phoenix.jdbc.PhoenixDriver","zkUrl",
+                "jdbc:phoenix:localhost:2181","table","RESULT"))
+        .save();
+         */
+
+     /*   System.out.println("Using phoenix Driver");
+        Map<String, String> options = new HashMap<String, String>();
+        options.put("zkUrl", "dfossouo-2.dfossouo.root.hwx.site:2181:/hbase");
+        options.put("table", "WEB_STAT");
+        Dataset<Row> df = spark.sqlContext().load("org.apache.phoenix.spark", options);
+        df.show();
+        System.out.println("End of phoenix Driver");*/
+
+
+
+
+
+    }
+}
